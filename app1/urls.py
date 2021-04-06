6582268537926b01f3e5779790b767bf58093e16
adarsh2805 <adarshag2805@gmail.com>
2021-04-06 20:57:06 +0530

    first commit

--- app1/urls.py
+++ app1/urls.py
@@ -5,6 +5,8 @@ app_name='app1'
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path("sample/",views.sample, name="sample")
+    path("sample/",views.sample, name="sample"),
+    path('<data>',views.carry_url,name='url_view'),
+    path('fact/<num>',views.fact,name='factorial')
     
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,8 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<center><h1 style="background-color:grey">httpresponse of apps1</h1></center>')
+    return HttpResponse('<center><h1 style="background-color:grey">httpresponse of apps1 project7</h1></center>')
 
 def sample(request):
     return render(request,'sample_app1.html')
+def carry_url(request,data):
+    return HttpResponse(f'<center><h1>you entered {data}</h1></center>')
+
+def fact(request,num):
+    a=int(num)
+    b=1
+    for i in range(int(num),1,-1):
+        b*=i
+    return HttpResponse(f'{b}')
+    
 
